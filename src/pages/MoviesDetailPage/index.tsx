40c1e5bc163403af793ca83b482c4334e6d22ce8
Reinoptland <reinoptland@gmail.com>
2020-09-10 17:16:18 +0200

    Detail Page

--- src/pages/MoviesDetailPage/index.tsx
+++ src/pages/MoviesDetailPage/index.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MoviesDetailPage() {
+  const [details, setDetails] = useState<any>({});
   const { imdbID } = useParams();
 
-  return <div>DETAILS!</div>;
+  useEffect(() => {
+    async function getMovieDetails() {
+      const response = await fetch(
+        `http://www.omdbapi.com/?i=${imdbID}&apikey=a7462395`
+      ).then((res) => res.json());
+
+      console.log(response);
+      setDetails(response);
+    }
+
+    getMovieDetails();
+  }, [imdbID]);
+
+  return (
+    <div>
+      <h1>{details.Title}</h1>
+      <img src={details.Poster} />
+      <p>{details.Plot}</p>
+    </div>
+  );
 }
