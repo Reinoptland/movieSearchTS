d2a416e1efd6e4c24720a4c57f6fe15c6df99927
Reinoptland <reinoptland@gmail.com>
2020-09-10 16:43:09 +0200

    Define Dynamic Route

---
+++ plan.md
@@ -0,0 +1,16 @@
+## Setup
+
+- De hele App wrappen in een Router
+
+## Static Routes
+
+- Routes definen & een component voor elke Route
+- Links maken met paths naar de juiste Routes
+
+## Dynamic Routes
+
+- Route definen met een route parameter
+- Dynamisch Links genereren waarin de parameter een waarde heeft
+
+- Route voor detail page voor een film -> parameter imdbID
+- Link genereren met imdbID van specifiek films (Discover page)
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
+import MoviesDetailPage from "./pages/MoviesDetailPage";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
       <Switch>
         <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/movies/:imdbID" component={MoviesDetailPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" exact component={HomePage} />
         <Route path="/" component={() => <div>404!</div>} />
